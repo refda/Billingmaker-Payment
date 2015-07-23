e38566a8f632ab588361f00e3442b7bb52574be8
refda <s@merq.de>
2015-07-23 14:42:37 +0200

    Update Wiederkehrend.php

--- Wiederkehrend.php
+++ Wiederkehrend.php
@@ -1,1 +1,7 @@
 <?php
+// ZU ERLEDIGEN:
+// 1. Ersetzen Sie den privaten Link durch Ihren eigenen Link, welchen Sie per E-Mail erhalten haben!
+// 2. Ersetzen Sie die ID des Vorganges. Die ID haben Sie zuvor über die programmgesteuerte Benachrichtigung erhalten.
+$privaterlink="http://www.beispiel.de/xyz?z=123";
+$idvorgang=11;
+file_get_contents($privaterlink."&del=".$idvorgang."&api=1");
