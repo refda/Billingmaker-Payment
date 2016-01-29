c45b0462e681cffccc126714acf8b3a7ec78246e
refda <s@merq.de>
2016-01-29 13:59:26 +0100

    JSON Obeject

--- Überprüfung.php
+++ Überprüfung.php
@@ -8,3 +8,21 @@ if ($json['name']!='') {
 //WEITERE AKKTIONEN
 
 }
+
+/* JSON Objekt, welches zurückgegeben wird.
+
+{
+  "name": "Vorname Nachname",
+  "iban": "DE00000000000000000000",
+  "ip": "0.0.0.0",
+  "datum": "2016-01-20 21:36:26",
+  "bezeichnung": "Rechnung 121",
+  "modus": "1",
+  "preis": "0,72",
+  "c": "custom-parameter",
+  "kstr": "Straße 12",
+  "kort": "11111 Musterstadt",
+  "ema": "mail@example.com"
+}
+
+*/
