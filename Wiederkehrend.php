09eb33c4f5b4ee912ab3b5369434989775665413
refda <s@merq.de>
2016-02-16 12:01:26 +0100

    Update Wiederkehrend.php

--- Wiederkehrend.php
+++ Wiederkehrend.php
@@ -1,7 +1,7 @@
 <?php
 // ZU ERLEDIGEN:
-// 1. Ersetzen Sie den privaten Link durch Ihren eigenen Link, welchen Sie per E-Mail erhalten haben!
-// 2. Ersetzen Sie die ID des Vorganges. Die ID haben Sie zuvor über die programmgesteuerte Benachrichtigung erhalten.
-$privaterlink="http://www.beispiel.de/xyz?z=123";
+// 1. Ersetzen Sie den API Link durch Ihren eigenen Link, welchen Sie per E-Mail erhalten haben!
+// 2. Ersetzen Sie die ID des zu löschenden Vorganges. Die ID haben Sie zuvor über die programmgesteuerte Benachrichtigung erhalten.
+$apilink="http://www.beispiel.de/xyz?z=123";
 $idvorgang=11;
-file_get_contents($privaterlink."&del=".$idvorgang."&api=1");
+file_get_contents($apilink."&del=".$idvorgang."&api=1");
