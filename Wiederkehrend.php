7c1c0db59b9d7776e32ee4d1c7590ed4a8b94c45
refda <s@merq.de>
2016-04-21 16:01:30 +0200

    Update Wiederkehrend.php

--- Wiederkehrend.php
+++ Wiederkehrend.php
@@ -1,7 +1,8 @@
 <?php
 // ZU ERLEDIGEN:
-// 1. Ersetzen Sie den API Link durch Ihren eigenen Link, welchen Sie per E-Mail erhalten haben!
+// 1. Ersetzen Sie die Händler ID und das Passwort.
 // 2. Ersetzen Sie die ID des zu löschenden Vorganges. Die ID haben Sie zuvor über die programmgesteuerte Benachrichtigung erhalten.
-$apilink="http://www.beispiel.de/xyz?z=123";
+$handler_id="0";
+$passwort="XYZ";
 $idvorgang=11;
-file_get_contents($apilink."&del=".$idvorgang."&api=1");
+file_get_contents("https://payment.billingmaker.com/sepa/privat/?i=".$handler_id."&h=".$passwort."&del=".$idvorgang."&api=1");
